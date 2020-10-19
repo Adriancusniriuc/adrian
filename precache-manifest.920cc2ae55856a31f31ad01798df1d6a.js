b11a4e0fe72ad9c11453f9b630ef34b9b8c05f58
Adriancusniriuc <cusniriuc.adrian05@gmail.com>
2020-10-19 15:46:10 +0200

    Updates

---
+++ precache-manifest.920cc2ae55856a31f31ad01798df1d6a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6d6d3a82a5d84acfa1cdc4ee8ebe53",
+    "url": "/adrian/index.html"
+  },
+  {
+    "revision": "595d304a7658a76c2342",
+    "url": "/adrian/static/css/main.17271c01.chunk.css"
+  },
+  {
+    "revision": "822c12cbe0643eb82546",
+    "url": "/adrian/static/js/2.f327d771.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/adrian/static/js/2.f327d771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595d304a7658a76c2342",
+    "url": "/adrian/static/js/main.9c5eeeb4.chunk.js"
+  },
+  {
+    "revision": "8051e4e63297776de0e1",
+    "url": "/adrian/static/js/runtime-main.498b17f8.js"
+  },
+  {
+    "revision": "da8e8432e55d66f511cea2ff37872047",
+    "url": "/adrian/static/media/BookClub.da8e8432.jpg"
+  },
+  {
+    "revision": "35195b8ee966a82e29e9e81ee96cfa6c",
+    "url": "/adrian/static/media/CrimeCheck.35195b8e.jpg"
+  },
+  {
+    "revision": "be255b64c796558ec87d374668d93465",
+    "url": "/adrian/static/media/Cuplu1.be255b64.jpg"
+  },
+  {
+    "revision": "4ce515e4ee83644258d461ac6e749c58",
+    "url": "/adrian/static/media/Provocarea.4ce515e4.jpg"
+  },
+  {
+    "revision": "1549cbda43f5978bbf5c498ce47307f3",
+    "url": "/adrian/static/media/Pubquiz.1549cbda.jpg"
+  },
+  {
+    "revision": "6e59dfbaf34a757a0de0a763f7f0a73f",
+    "url": "/adrian/static/media/Snake.6e59dfba.jpg"
+  },
+  {
+    "revision": "a6b1dd490461621b7031dd5f8daf1740",
+    "url": "/adrian/static/media/Zanora.a6b1dd49.jpg"
+  },
+  {
+    "revision": "82b891f148d6056cc8b3afc9495c7edc",
+    "url": "/adrian/static/media/ecommerce.82b891f1.jpg"
+  },
+  {
+    "revision": "52ee660e327a15f063e053bf61067322",
+    "url": "/adrian/static/media/header-background.52ee660e.jpg"
+  },
+  {
+    "revision": "7087a5510f1ef84f36a2a06c1de51de3",
+    "url": "/adrian/static/media/profilepic.7087a551.jpg"
+  }
+]);
